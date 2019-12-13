34738be5efbee080d08d63a7015cc928a776fd7f
syall <s.yuan.all@gmail.com>
2019-12-13 01:13:22 -0500

    Scaffold Parser

--- bin/parser.js
+++ bin/parser.js
@@ -1,4 +1,34 @@
-class Parser {
+export default class DomyParser {
+
+    constructor() {
+        // Record of all Parses
+        this.record = [];
+    }
+
+    parse(tokens) {
+        // State
+        let i = 0;
+
+        // Parsing Functions
+        const statements = () => {
+            while (i < tokens.length)
+                i++;
+            return [{ one: 1 }, { two: 2 }];
+        };
+
+        // Parse Statements
+        const parsed = statements();
+
+        // Save to Record
+        this.record.push(parsed);
+
+        // Return Statements
+        return parsed;
+    }
+
+    toString() {
+        for (const record of this.record)
+            console.log(JSON.stringify(record, null, 2));
+    }
 
 }
-export default Parser;
